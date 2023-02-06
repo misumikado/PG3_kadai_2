4e9bf29b7ebfc224c07e7a96f72d63f7f87a4a48
MSI\k021g <k021g1161@g.neec.ac.jp>
2023-02-06 16:39:57 +0900

    3_3完成

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,12 +1,52 @@
 ﻿#include "Enemy.h"
+#include <stdio.h>
 
-void Enemy::SetAlive(bool isAlive)
+void (Enemy::* Enemy::pFuncTable[])() =
 {
-	isAlive_ = isAlive;
+	&Enemy::Approach,
+	&Enemy::Shoot,
+	&Enemy::GetAway
+};
+
+Enemy::Enemy()
+{
+	phase_ = 0;
 }
 
-void Enemy::Print()
+Enemy::~Enemy()
 {
-	if (isAlive_) { printf("生存\n"); }
-	else { printf("消滅\n"); }
+}
+
+void Enemy::Approach()
+{
+	printf("近接\n");
+}
+
+void Enemy::Shoot()
+{
+	printf("射撃\n");
+}
+
+void Enemy::GetAway()
+{
+	printf("離脱\n");
+}
+
+void Enemy::Update()
+{
+	printf("phase_ : %d\n", phase_);
+
+	(this->*pFuncTable[phase_])();
+
+	phase_++;
+
+	if (phase_ == 3)
+	{
+		phase_ = 0;
+	}
+}
+
+void Enemy::Draw()
+{
+	printf("(enemy draw)\n");
 }
