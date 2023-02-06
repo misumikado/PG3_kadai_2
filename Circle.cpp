fd60ea08fdd8a3ef3eb72a3fc871d85362765860
MSI\k021g <k021g1161@g.neec.ac.jp>
2023-02-06 21:44:40 +0900

    3_6

---
+++ Circle.cpp
@@ -0,0 +1,12 @@
+﻿#include "Circle.h"
+#include <stdio.h>
+
+void Circle::Size()
+{
+	printf("‰~‚Ì–ÊÏ‚ð‹‚ß‚é\n");
+}
+
+void Circle::Draw()
+{
+	printf("Z\n");
+}
---
+++ Rectangle.cpp
@@ -0,0 +1,12 @@
+﻿#include "Rectangle.h"
+#include <stdio.h>
+
+void Rectangle::Size()
+{
+	printf("ŽlŠp‚Ì–ÊÏ‚ð‹‚ß‚é\n");
+}
+
+void Rectangle::Draw()
+{
+	printf(" \n");
+}
